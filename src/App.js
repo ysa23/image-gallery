84782cbf666f582b4fc513ac1246d7c93d97fd6f
ysa23 <ysa.angel@gmail.com>
2018-12-28 10:39:04 +0200

    Revise App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ImageTile from './ImageTile';
+import ImageStorage from './imageStorage';
+import classNames from 'classnames';
+import styles from './App.module.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.images = ImageStorage.getImages(0);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={classNames(styles.app)}>
+          <header className={classNames(styles.header)}>Image Gallery</header>
+          <div className={classNames(styles.images)}>
+              {this.images.images.map(function(image, index) {
+                return <ImageTile image={image} onClick={console.log(image.id)}/>
+              })}
+          </div>
       </div>
     );
   }
