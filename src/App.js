9ae82d3d5d28cff93b589778c51b0bfadd10bd98
ysa23 <ysa.angel@gmail.com>
2018-12-29 19:04:52 +0200

    UI adjustments

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ImageTile from './ImageTile';
 import ImageStorage from './imageStorage';
 import classNames from 'classnames';
 import styles from './App.module.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.images = ImageStorage.getImages(0);
-  }
+	constructor(props) {
+		super(props);
+		this.images = ImageStorage.getImages(0);
+	}
 
-  render() {
-    return (
-      <div className={classNames(styles.app)}>
-          <header className={classNames(styles.header)}>Image Gallery</header>
-          <div className={classNames(styles.images)}>
-              {this.images.images.map(function(image, index) {
-                return <ImageTile image={image} onClick={console.log(image.id)}/>
-              })}
-          </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className={classNames(styles.app)}>
+				<header className={classNames(styles.header)}>Image Gallery</header>
+				<div className={classNames(styles.images)}>
+					{this.images.images.map(function (image, index) {
+						return <ImageTile image={image} onClick={console.log(image.id)}/>
+					})}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
