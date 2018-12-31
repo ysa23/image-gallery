f7d37c8b193e511f927be1b6e196439cb0958972
ysa23 <ysa.angel@gmail.com>
2018-12-31 18:00:14 +0200

    Extract methods

--- src/store/reducers/gallery.js
+++ src/store/reducers/gallery.js
@@ -4,48 +4,68 @@ import {APP_INIT, IMAGE_DESCRIPTION_UPDATED, IMAGE_DELETED, IMAGE_PAGE_INIT, IMA
 const initialState = {
 	images: [],
 	currentImage: null
-}
+};
 
 export default function(state = initialState, action) {
 	switch (action.type) {
 		case APP_INIT:
-			return {
-				...state,
-				images: action.images
-			};
+			return appInit(state, action);
 
 		case IMAGE_PAGE_INIT:
-			return {
-				...state,
-				currentImage: action.image
-			};
+			return imagePageInit(state, action);
 
 		case IMAGE_DESCRIPTION_UPDATED:
-			return {
-				...state,
-				currentImage: Object.assign(state.currentImage, { description: action.newDescription }),
-				images: state.images.map(x => {
-					if (x.id === action.imageId)
-						return Object.assign({}, x, { description: action.newDescription})
-					return x;
-				})
-			};
+			return imageDescriptionUpdated(state, action);
 
 		case IMAGE_DELETED:
-			return {
-				...state,
-				currentImage: null,
-				images: state.images.filter(x => x.id !== action.imageId)
-			};
+			return imageDeleted(state, action);
 
 		case IMAGE_PAGE_EXIT:
-			return {
-				...state,
-				currentImage: null
-			};
+			return imagePageExit(state);
 
 		default:
 			console.warn(`Non existing action triggered: ${JSON.stringify(action)}`);
 			return state;
 	}
+}
+
+function appInit(state, action) {
+	return {
+		...state,
+		images: action.images
+	};
+}
+
+function imagePageInit(state, action) {
+	return {
+		...state,
+		currentImage: action.image
+	};
+}
+
+function imageDescriptionUpdated(state, action) {
+	return {
+		...state,
+		currentImage: Object.assign(state.currentImage, {description: action.newDescription}),
+		images: state.images.map(x => {
+			if (x.id === action.imageId)
+				return Object.assign({}, x, {description: action.newDescription})
+			return x;
+		})
+	};
+}
+
+function imageDeleted(state, action) {
+	return {
+		...state,
+		currentImage: null,
+		images: state.images.filter(x => x.id !== action.imageId)
+	};
+}
+
+function imagePageExit(state) {
+	return {
+		...state,
+		currentImage: null
+	};
 }
