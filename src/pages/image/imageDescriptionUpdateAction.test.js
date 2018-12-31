8c044493fca49be870906f17f9bc277027c65bdc
ysa23 <ysa.angel@gmail.com>
2018-12-31 13:19:11 +0200

    Add tests for image description update action

---
+++ src/pages/image/imageDescriptionUpdateAction.test.js
@@ -0,0 +1,20 @@
+import target from './imageDescriptionUpdateAction';
+
+import { IMAGE_DESCRIPTION_UPDATED } from '../../store/actionTypes';
+import storage from '../../api/imagesApi';
+jest.mock('../../api/imagesApi');
+
+test('imageDescriptionUpdateAction, when called, returns IMAGE_DESCRIPTION_UPDATE action type with new description and update the server from', () => {
+
+	const result = target(1, "new description");
+
+	expect(result.type).toEqual(IMAGE_DESCRIPTION_UPDATED);
+	expect(result.imageId).toEqual(1);
+	expect(result.newDescription).toEqual("new description");
+
+	expect(storage.updateImageDescription).toBeCalledWith(1, "new description");
+});
+
+function setImage(image) {
+	storage.getImage.mockImplementationOnce(id => image);
+}
