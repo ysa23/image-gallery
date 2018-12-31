c9ff9d0bd51b1a2f0ec664782add3bdf738224de
ysa23 <ysa.angel@gmail.com>
2018-12-31 11:56:10 +0200

    Oopppss missed a file

---
+++ src/pages/image/imageDeleteAction.js
@@ -0,0 +1,11 @@
+import imagesApi from '../../imagesApi';
+import { IMAGE_DELETED } from '../../store/actionTypes';
+
+export default (imageId) => {
+	imagesApi.deleteImage(imageId);
+
+	return {
+		type: IMAGE_DELETED,
+		imageId: imageId
+	};
+}
